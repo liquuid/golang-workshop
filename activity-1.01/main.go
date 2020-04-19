a51e275665891064487af92b6c3b4f54a2b49849
Fernando Henrique <liquuid@gmail.com>
2020-04-19 01:14:59 -0300

    initial

---
+++ activity-1.01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fname := "Bob"
+	sname := "Smith"
+	age := 24
+	peanut_allergy := false
+
+	fmt.Println(fname)
+	fmt.Println(sname)
+	fmt.Println(age)
+	fmt.Println(peanut_allergy)
+
+}
